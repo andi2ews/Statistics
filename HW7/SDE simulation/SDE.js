71643849fc0bf2a617acb1a411888939e0cea608
andi2ews <148214491+andi2ews@users.noreply.github.com>
2023-11-21 18:27:27 +0100

    Delete HW7 directory

--- HW7/SDE simulation/SDE.js
+++
@@ -1,7 +0,0 @@
-const SDE = {
-    Cox_Ingersoll_Ross: "CIR",
-    Geometric_Brownian: "geometric_brownian",
-    Hull_White: "hull_white",
-    Ornstein_Uhlenbeck: "OU",
-    Vasicek: "vasicek",
-}
